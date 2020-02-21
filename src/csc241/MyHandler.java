760e8d116ff24997a08639935e2fcd8fd72450c5
JamesPEarly <james.early@oswego.edu>
2020-02-21 11:19:03 -0500

    Handle month element

--- src/csc241/MyHandler.java
+++ src/csc241/MyHandler.java
@@ -5,9 +5,16 @@ import org.xml.sax.helpers.*;
 
 public class MyHandler extends DefaultHandler {
 
+    private String currentMonth = null;
+
     public void startElement(String uri, String localName, String qName, Attributes attributes)
             throws SAXException {
-        // we’ll add more later
+        // Handle the "month" element
+        if (qName.equals("month")){
+            // store the "name" attribute for use across all method calls
+            currentMonth = attributes.getValue("name");
+            System.out.println("CURRENTMONTH:" + currentMonth);
+        }
     }
 
 }
